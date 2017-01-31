369d9066773891fb7f66364e54f78dd305edaca6
belly <abdelrhmanalaa39@gmail.com>
2017-01-31 11:03:02 +0000

    add email functionality for signup and styling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,8 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'https://ruby-on-rails-belly.c9users.io'}
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
@@ -26,6 +28,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.debug = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'belly-photo-app.herokuapp.com', :protocol => 'https'}
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
